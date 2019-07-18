5976f15acc66b6dec7fb1e8fac729f723a611093
S Bademiya <00001613@zen3.com>
2019-07-18 12:47:11 +0530

    prod route

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const axios = require('axios');
-require('./routes/startup')(app);
-if (process.env.NODE_ENV === 'production') {
-	console.log('App running in production-------------');
-	//Express will serve up production assets
-	//like our main.js file or main.css file
-	const path = require('path');
-	// app.use(express.static(path.join(__dirname, 'client/build')));
-	app.use(express.static('client/build'));
-	//Express will serve up the index.html
-	//it it does not recognize the route
+const axios = require("axios");
+require("./routes/startup")(app);
+if (process.env.NODE_ENV === "production") {
+  console.log("App running in production-------------");
+  //Express will serve up production assets
+  //like our main.js file or main.css file
+  const path = require("path");
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
+  //Express will serve up the index.html
+  //it it does not recognize the route
 
-	app.get('*', (req, res) => {
-		console.log('Entered production route-------------');
-		res.sendFile(
-			path.resolve(__dirname, 'client', 'build', 'index.html')
-		);
-	});
+  app.get("/*", (req, res) => {
+    console.log("Entered production route-------------");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	console.log('App listening on PORT : ', PORT);
+  console.log("App listening on PORT : ", PORT);
 });
